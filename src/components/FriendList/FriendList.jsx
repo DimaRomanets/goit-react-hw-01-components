ffb76353ace404ee85b84b5ac45e939befe7b517
DimaRomanets <130175375+DimaRomanets@users.noreply.github.com>
2023-10-14 17:30:49 +0200

    okey

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,12 +1,27 @@
-import css from './FriendList.module.css';
+import PropTypes from "prop-types";
+import FriendListItem from "./FriendListItem/FriendListItem";
 
+export default function FriendList({ friends }) {
+  return (
+    <ul>
+      {friends.map(({ id, isOnline, avatar, name }) => (
+        <FriendListItem
+          key={id}
+          isOnline={isOnline}
+          avatar={avatar}
+          name={name}
+        />
+      ))}
+    </ul>
+  );
+}
 
-export const FriendList = ({ title, price, discount }) => {
-    
-  
-    return (
-      <div className={css.name} >
-        
-      </div>
-    );
-  };
+FriendList.prototype = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      isOnline: PropTypes.bool,
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+    })
+  ).isRequired,
+};
